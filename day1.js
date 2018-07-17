f92ff4316b23a079883adf38f780816f38f8c755
AspiringDevsLog <40519130+aspiringdevslog@users.noreply.github.com>
2018-07-18 06:56:01 +0800

    Day 1: Data Types

---
+++ day1.js
@@ -0,0 +1,16 @@
+// Declare second integer, double, and String variables.
+var i2, d2, s2; 
+// Read and save an integer, double, and String to your variables.
+i2 = readLine();
+d2 = readLine();
+s2 = readLine();
+// Print the sum of both integer variables on a new line.
+console.log(parseInt(i) + parseInt(i2));
+// Print the sum of the double variables on a new line.
+console.log((parseFloat(d) + parseFloat(d2)).toFixed(1));
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + s2);
+
+
+// https://gomakethings.com/converting-strings-to-numbers-with-vanilla-javascript/
