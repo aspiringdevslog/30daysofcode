c253e1beeb216b44261313976942d702b256b8c7
AspiringDevsLog <40519130+aspiringdevslog@users.noreply.github.com>
2018-07-20 06:52:22 +0800

    30 days of code: redo in python 3

---
+++ day1.py
@@ -0,0 +1,14 @@
+# https://www.digitalocean.com/community/tutorials/how-to-convert-data-types-in-python-3
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+i2 = input()
+d2 = input()
+s2 = input()
+# Print the sum of both integer variables on a new line.
+print(int(i)+int(i2))
+# Print the sum of the double variables on a new line.
+print(float(d)+float(d2))
+# Concatenate and print the String variables on a new line
+print(s+s2)
+# The 's' variable above should be printed first.
