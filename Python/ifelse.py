f00164254f18cf28e3494179161f365a85b191f6
AspiringDevsLog <40519130+aspiringdevslog@users.noreply.github.com>
2018-07-20 09:29:25 +0800

    some python practice

---
+++ Python/ifelse.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+
+if N%2:
+    print("Weird")
+elif N>20:
+    print("Not Weird")
+elif N<=5 & N>=2:
+    print("Not Weird")
+else:
+    print("Weird")    
+
