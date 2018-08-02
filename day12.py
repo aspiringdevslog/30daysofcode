06e7dba53a960c87f9d0fd377a6bbfa5e80ef5cb
AspiringDevsLog <40519130+aspiringdevslog@users.noreply.github.com>
2018-08-02 20:18:44 +0800

    Day 12: inheritance
    I'm pretty bad at OOP, right now the code is based on https://github.com/ehouarn-perret/EhouarnPerret.Python.HackerRank/blob/master/0%20-%20Tutorials/30%20Days%20of%20Code/Day%2012%20-%20Inheritance.py
    
    I don't quite understand it. need to spend some time looking through this code

---
+++ day12.py
@@ -0,0 +1,37 @@
+class Student(Person):
+    #   Class Constructor
+        def __init__(self, firstName, lastName, idNumber, scores):
+            super().__init__(firstName, lastName, idNumber)
+            self.scores = scores
+    
+    
+    
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+        def calculate(self):
+            a = sum(self.scores) / len(self.scores)
+            if a < 40:
+                return "T"
+            elif (40 <= a) and (a < 55):
+                return "D"
+            elif (55 <= a) and (a < 70):
+                return "P"
+            elif (70 <= a) and (a < 80):
+                return "A"
+            elif (80 <= a) and (a < 90):
+                return "E"
+            elif (90 <= a) and (a <= 100):
+                return "O"
+            else:
+                return ""
