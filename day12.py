64201f17973435d9ad1242fa5d339b922ac93ff6
AspiringDevsLog <40519130+aspiringdevslog@users.noreply.github.com>
2018-08-08 17:15:55 +0800

    Day 12
    Apparently the error is due to the print function given

--- day12.py
+++ day12.py
@@ -1,3 +1,14 @@
+print_object = print
+
+def print(*items_to_print):
+    if 'Grade: ' in items_to_print:
+        items_to_print = map(str, items_to_print)
+        
+        print_object(''.join(items_to_print))
+    else:
+        print_object(*items_to_print)
+
+
 class Student(Person):
     #   Class Constructor
         def __init__(self, firstName, lastName, idNumber, scores):
